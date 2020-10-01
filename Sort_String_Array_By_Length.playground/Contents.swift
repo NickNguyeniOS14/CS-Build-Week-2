ff031ac71488a9ca4c1f566934a629be291c64c1
Nick <ptnguyen1901@gmail.com>
2020-10-01 13:58:32 -0400

    missing numbers from array

--- Sort_String_Array_By_Length.playground/Contents.swift
+++ Sort_String_Array_By_Length.playground/Contents.swift
@@ -1,13 +1,23 @@
 import UIKit
 
-extension Collection where Element == String {
-    func challenge39() -> [String] {
-        return self.sorted { $0.count > $1.count}
+func challenge40a(input: [Int]) -> [Int] {
+    let correctArray = Array(1...100)
+    var missingNumbers = [Int]()
+
+    for number in correctArray {
+        if !input.contains(number) {
+            missingNumbers.append(number)
+        }
     }
+    return missingNumbers
 }
 
 
+challenge40a(input: Array(1...95))
 
-["a","abc","ab"].challenge39()
-["paul", "taylor", "adele"].challenge39()
-
+func challenge40c(input: [Int]) -> [Int] {
+    let inputSet = Set(input)
+    let testSet = Set(1...100)
+    return Array(testSet.subtracting(inputSet)).sorted()
+}
+challenge40c(input: Array(2...98))
