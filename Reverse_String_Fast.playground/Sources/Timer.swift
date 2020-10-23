61f2e3e7cc5e2ed9ff86235d427130b843573a90
Nick <ptnguyen1901@gmail.com>
2020-10-23 10:29:47 -0400

    reverse_string_backward_loop

---
+++ Reverse_String_Fast.playground/Sources/Timer.swift
@@ -0,0 +1,24 @@
+import Foundation
+public class ParkBenchTimer {
+
+    let startTime:CFAbsoluteTime
+    var endTime:CFAbsoluteTime?
+
+    public init() {
+        startTime = CFAbsoluteTimeGetCurrent()
+    }
+
+    public func stop() -> CFAbsoluteTime {
+        endTime = CFAbsoluteTimeGetCurrent()
+
+        return duration!
+    }
+
+    public var duration:CFAbsoluteTime? {
+        if let endTime = endTime {
+            return endTime - startTime
+        } else {
+            return nil
+        }
+    }
+}
